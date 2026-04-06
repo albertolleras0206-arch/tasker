f966d17dad37e89f5a69d4135dcdbe4b78aa2329
Alberto <albertolleras0206@gmail.com>
2026-04-06 17:05:47 -0500

    Connection to Mongo DB successfull

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
